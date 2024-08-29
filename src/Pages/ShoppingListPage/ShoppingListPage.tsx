47e29aa8bc6202d00961701d091cac1cec33024f
Sven Ole Spindler <s.spindler@gmx.de>
2024-08-29 14:26:41 +0200

    styled confirmation for delete with daisy ui modal

--- src/Pages/ShoppingListPage/ShoppingListPage.tsx
+++ src/Pages/ShoppingListPage/ShoppingListPage.tsx
@@ -29,9 +29,40 @@ const ShoppingListPage: React.FC = () => {
     string | null
   >(null); // State for selected user to unshare
   const [sharedUsers, setSharedUsers] = useState<User[]>([]); // State for shared users
+  const [listToDelete, setListToDelete] = useState<string | null>(null); // State for list to delete
   const authContext = useContext(AuthContext); // Get AuthContext
   const currentUser = authContext?.user; // Get current user from AuthContext
   const navigate = useNavigate();
+
+  const fetchUsersAndSharedUsers = async () => {
+    if (!currentUser) return; // Add type guard here
+
+    try {
+      const [usersResponse, sharedUsersResponse] = await Promise.all([
+        axios.get(`${import.meta.env.VITE_SERVER_URL}/users`),
+        axios.get(
+          `${
+            import.meta.env.VITE_SERVER_URL
+          }/shopping-lists/${shoppingListId}/shared-users`
+        ),
+      ]);
+
+      const allUsers = usersResponse.data;
+      const sharedUsers = sharedUsersResponse.data;
+
+      // Filter out the current user and shared users
+      const filteredUsers = allUsers.filter(
+        (user: User) =>
+          user.id !== currentUser.id &&
+          !sharedUsers.some((sharedUser: User) => sharedUser.id === user.id)
+      );
+
+      setUsers(filteredUsers);
+      setSharedUsers(sharedUsers);
+    } catch (error) {
+      console.error("Error fetching users or shared users:", error);
+    }
+  };
 
   useEffect(() => {
     if (!currentUser) {
@@ -47,34 +78,6 @@ const ShoppingListPage: React.FC = () => {
         setShoppingList(response.data);
       } catch (error) {
         console.error("Error fetching shopping list:", error);
-      }
-    };
-
-    const fetchUsersAndSharedUsers = async () => {
-      try {
-        const [usersResponse, sharedUsersResponse] = await Promise.all([
-          axios.get(`${import.meta.env.VITE_SERVER_URL}/users`),
-          axios.get(
-            `${
-              import.meta.env.VITE_SERVER_URL
-            }/shopping-lists/${shoppingListId}/shared-users`
-          ),
-        ]);
-
-        const allUsers = usersResponse.data;
-        const sharedUsers = sharedUsersResponse.data;
-
-        // Filter out the current user and shared users
-        const filteredUsers = allUsers.filter(
-          (user: User) =>
-            user.id !== currentUser.id &&
-            !sharedUsers.some((sharedUser: User) => sharedUser.id === user.id)
-        );
-
-        setUsers(filteredUsers);
-        setSharedUsers(sharedUsers);
-      } catch (error) {
-        console.error("Error fetching users or shared users:", error);
       }
     };
 
@@ -150,13 +153,15 @@ const ShoppingListPage: React.FC = () => {
 
   const handleUnshareShoppingList = async () => {
     if (!selectedUnshareUserId) return;
-  
+
     try {
       await axios.delete(
-        `${import.meta.env.VITE_SERVER_URL}/user-shopping-list-links/${selectedUnshareUserId}/${shoppingListId}`
+        `${
+          import.meta.env.VITE_SERVER_URL
+        }/user-shopping-list-links/${selectedUnshareUserId}/${shoppingListId}`
       );
       console.log("Shopping list unshared successfully");
-  
+
       // Refetch users and shared users after unsharing the shopping list
       fetchUsersAndSharedUsers();
     } catch (error) {
@@ -164,47 +169,27 @@ const ShoppingListPage: React.FC = () => {
     }
   };
 
-  const handleDeleteList = async () => {
-    const confirmed = window.confirm("Are you sure you want to delete this list?");
-    if (!confirmed) return;
-  
-    try {
-      await axios.delete(
-        `${import.meta.env.VITE_SERVER_URL}/shopping-lists/${shoppingListId}`
-      );
-      navigate('/'); // Navigate back to the home page after deletion
-    } catch (error) {
-      console.error("Error deleting shopping list:", error);
-    }
+  const handleDeleteList = () => {
+    setListToDelete(shoppingListId || null);
+    (
+      document.getElementById("delete_list_modal") as HTMLDialogElement
+    )?.showModal();
   };
 
-  const fetchUsersAndSharedUsers = async () => {
-    if (!currentUser) return; // Add type guard here
+  const confirmDeleteList = async () => {
+    if (!listToDelete) return;
 
     try {
-      const [usersResponse, sharedUsersResponse] = await Promise.all([
-        axios.get(`${import.meta.env.VITE_SERVER_URL}/users`),
-        axios.get(
-          `${
-            import.meta.env.VITE_SERVER_URL
-          }/shopping-lists/${shoppingListId}/shared-users`
-        ),
-      ]);
-
-      const allUsers = usersResponse.data;
-      const sharedUsers = sharedUsersResponse.data;
-
-      // Filter out the current user and shared users
-      const filteredUsers = allUsers.filter(
-        (user: User) =>
-          user.id !== currentUser.id &&
-          !sharedUsers.some((sharedUser: User) => sharedUser.id === user.id)
+      await axios.delete(
+        `${import.meta.env.VITE_SERVER_URL}/shopping-lists/${listToDelete}`
       );
-
-      setUsers(filteredUsers);
-      setSharedUsers(sharedUsers);
+      navigate("/"); // Navigate back to the home page after deletion
+      (
+        document.getElementById("delete_list_modal") as HTMLDialogElement
+      )?.close();
+      setListToDelete(null);
     } catch (error) {
-      console.error("Error fetching users or shared users:", error);
+      console.error("Error deleting shopping list:", error);
     }
   };
 
@@ -214,113 +199,138 @@ const ShoppingListPage: React.FC = () => {
 
   return (
     <div className="px-4 pb-4">
-    <div className="card prose py-4 px-5 m- w-full bg-base-200">
-      <h2 className="w-full text-center">{shoppingList.name}</h2>
-      <div>
-        {shoppingList.items.map((item) => (
-          <div
-            key={item.id}
-            className="my-2 join place-items-center w-full text-left"
-          >
-            <div className="input input-bordered input-neutral w-full join-item flex items-center justify-start">
-              {item.name}{" "}
-              {/* {item.quantity && `- Quantity: ${item.quantity}`} */}
-            </div>
-            <button
-              className="btn join-item btn-neutral"
-              onClick={() => handleDeleteItem(item.id)}
-            >
-              DELETE ITEM
-            </button>
-          </div>
-        ))}
-      </div>
-      <div className="join w-full my-2">
-        <input
-          className="input input-bordered input-primary w-full join-item"
-          placeholder="Type here"
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-        />
-        <button
-          className="btn join-item btn-primary"
-          onClick={handleAddItem}
-        >
-          ADD ITEM
-        </button>
-      </div>
-
-      <div className="join w-full my-2 max-w-xs">
-        <button
-          className="btn join-item btn-neutral btn-sm"
-          onClick={() => navigate(-1)}
-        >
-          Go Back
-        </button>
-        <button
-          className="btn join-item btn-error btn-sm"
-          onClick={handleDeleteList}
-        >
-          Delete List
-        </button>
-      </div>
-
-      <div className="flex w-full flex-col">
-        <div className="divider">Sharing Options</div>
-      </div>
-      <div className="join w-full my-2">
-        <select
-          className="select select-bordered select-secondary w-full join-item"
-          onChange={(e) => setSelectedUserId(e.target.value)}
-        >
-          <option disabled selected>
-            Choose User
-          </option>
-          {users.map((user) => (
-            <option key={user.id} value={user.id}>
-              {user.name}
-            </option>
-          ))}
-        </select>
-        <button
-          className="btn join-item btn-secondary"
-          onClick={handleShareShoppingList}
-        >
-          SHARE LIST
-        </button>
-      </div>
-
-      <div className="join w-full my-2">
-        <select
-          className="select select-bordered select-secondary w-full join-item"
-          onChange={(e) => setSelectedUnshareUserId(e.target.value)}
-        >
-          <option disabled selected>
-            Choose User to Unshare
-          </option>
-          {sharedUsers.map((user) => (
-            <option key={user.id} value={user.id}>
-              {user.name}
-            </option>
-          ))}
-        </select>
-        <button
-          className="btn join-item btn-secondary"
-          onClick={handleUnshareShoppingList}
-        >
-          UNSHARE LIST
-        </button>
-      </div>
-
-      <div className="mt-4">
-        <h3>Shared with:</h3>
+      <div className="card prose py-4 px-5 m- w-full bg-base-200">
+        <h2 className="w-full text-center">{shoppingList.name}</h2>
         <div>
-          {sharedUsers.map((user) => (
-            <label key={user.id}>{user.name}</label>
+          {shoppingList.items.map((item) => (
+            <div
+              key={item.id}
+              className="my-2 join place-items-center w-full text-left"
+            >
+              <div className="input input-bordered input-neutral w-full join-item flex items-center justify-start">
+                {item.name}{" "}
+                {/* {item.quantity && `- Quantity: ${item.quantity}`} */}
+              </div>
+              <button
+                className="btn join-item btn-neutral"
+                onClick={() => handleDeleteItem(item.id)}
+              >
+                DELETE ITEM
+              </button>
+            </div>
           ))}
         </div>
+        <div className="join w-full my-2">
+          <input
+            className="input input-bordered input-primary w-full join-item"
+            placeholder="Type here"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+          <button className="btn join-item btn-primary" onClick={handleAddItem}>
+            ADD ITEM
+          </button>
+        </div>
+
+        <div className="join w-full my-2 max-w-xs">
+          <button
+            className="btn join-item btn-neutral btn-sm"
+            onClick={() => navigate(-1)}
+          >
+            Go Back
+          </button>
+          <button
+            className="btn join-item btn-error btn-sm"
+            onClick={handleDeleteList}
+          >
+            Delete List
+          </button>
+        </div>
+
+        <div className="flex w-full flex-col">
+          <div className="divider">Sharing Options</div>
+        </div>
+        <div className="join w-full my-2">
+          <select
+            className="select select-bordered select-secondary w-full join-item"
+            onChange={(e) => setSelectedUserId(e.target.value)}
+          >
+            <option disabled selected>
+              Choose User
+            </option>
+            {users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          <button
+            className="btn join-item btn-secondary"
+            onClick={handleShareShoppingList}
+          >
+            SHARE LIST
+          </button>
+        </div>
+
+        <div className="join w-full my-2">
+          <select
+            className="select select-bordered select-secondary w-full join-item"
+            onChange={(e) => setSelectedUnshareUserId(e.target.value)}
+          >
+            <option disabled selected>
+              Choose User to Unshare
+            </option>
+            {sharedUsers.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+          <button
+            className="btn join-item btn-secondary"
+            onClick={handleUnshareShoppingList}
+          >
+            UNSHARE LIST
+          </button>
+        </div>
+
+        <div className="mt-4">
+          <h3>Shared with:</h3>
+          <div>
+            {sharedUsers.map((user) => (
+              <label key={user.id}>{user.name}</label>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+
+      {/* Modal */}
+      <dialog
+        id="delete_list_modal"
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Confirm Delete</h3>
+          <p className="py-4">Are you sure you want to delete this list?</p>
+          <div className="modal-action">
+            <button
+              className="btn"
+              onClick={() =>
+                (
+                  document.getElementById(
+                    "delete_list_modal"
+                  ) as HTMLDialogElement
+                )?.close()
+              }
+            >
+              Cancel
+            </button>
+            <button className="btn btn-primary" onClick={confirmDeleteList}>
+              Confirm
+            </button>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
